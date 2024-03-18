3527ec2ae4f316e4c6dc1bcb886f961f667c9a59
tmachine <tmachine@sys08.PEC.IN>
2024-03-18 09:43:17 +0530

    image alters

--- tutorial2.py
+++ tutorial2.py
@@ -4,9 +4,14 @@ import random
 img=cv2.imread('assets/image.jpg',1)
 img=cv2.resize(img,(800,500))
 
-for i in range(0,len(img)-100):
-    for j in range(img.shape[1]):
-        img[i][j]=[random.randint(0,255),random.randint(0,255),random.randint(0,255)]
+
+tag=img[100:200, 200:500]
+img[0:100,0:300]=tag
+
+# for i in range(0,len(img)-100):
+#     for j in range(img.shape[1]):
+#         img[i][j]=[random.randint(0,255),random.randint(0,255),random.randint(0,255)]
+
 
 cv2.imshow('image',img)
 cv2.waitKey(0)

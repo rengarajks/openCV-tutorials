e2996977c46cb1f6ab83ee01dc5c05286a6d11b0
tmachine <tmachine@sys08.PEC.IN>
2024-03-18 09:40:05 +0530

    image alter

--- tutorial1.py
+++ tutorial1.py
@@ -1,7 +1,12 @@
 import cv2
 
-img=cv2.imread('assets/image.jpg',0)
+img=cv2.imread('assets/image.jpg',-1)
 img=cv2.resize(img,(0,0),fx=0.25,fy=0.25)
+img=cv2.rotate(img,cv2.ROTATE_90_CLOCKWISE)
+
+
+#save
+cv2.imwrite('new_image.jpg',img)
 
 cv2.imshow('image 1',img)
 cv2.waitKey(0)
---
+++ tutorial2.py
@@ -0,0 +1,13 @@
+import cv2
+import random
+
+img=cv2.imread('assets/image.jpg',1)
+img=cv2.resize(img,(800,500))
+
+for i in range(0,len(img)-100):
+    for j in range(img.shape[1]):
+        img[i][j]=[random.randint(0,255),random.randint(0,255),random.randint(0,255)]
+
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
